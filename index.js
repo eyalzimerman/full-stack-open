c36c467b3cd38df2fb2bc00b278ce6dae98211b7
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-07 16:00:53 +0200

    step 1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+let phoneBook = [
+  {
+    id: 1,
+    name: "eyal",
+    number: "0501230773",
+  },
+  {
+    id: 2,
+    name: "yair",
+    number: "0502340773",
+  },
+  {
+    id: 3,
+    name: "jino",
+    number: "0506780773",
+  },
+  {
+    id: 4,
+    name: "gil",
+    number: "0508880773",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(phoneBook);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
