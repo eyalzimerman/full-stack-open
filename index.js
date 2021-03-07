a1599c3ec0672b0220f6fea75b823af22d4493df
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-08 00:11:17 +0200

    add get index.html

--- index.js
+++ index.js
@@ -96,6 +96,12 @@ app.post("/api/persons/", (request, response) => {
   response.send(phoneBook);
 });
 
+app.use("/", express.static(`./public`));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "./public/index.html");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
