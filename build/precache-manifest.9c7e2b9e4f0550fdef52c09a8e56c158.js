8e8c6acf1f91e146149e32b2250bb5fd451e3a38
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-08 00:06:48 +0200

    heroku

---
+++ build/precache-manifest.9c7e2b9e4f0550fdef52c09a8e56c158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846056c696fb080edbd868ee325f809b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc26491fd43752fc742",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "1224e9f809dff3239121",
+    "url": "/static/js/2.870ca22c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.870ca22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc26491fd43752fc742",
+    "url": "/static/js/main.652a4aba.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
