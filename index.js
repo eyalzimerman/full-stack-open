217137077d614bf0e9e1318764a45c20517c6952
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-08 15:04:20 +0200

    step 12 mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -106,3 +107,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = getRandomId;
---
+++ mongo.js
@@ -0,0 +1,51 @@
+const { bodyParser } = require("json-server");
+const mongoose = require("mongoose");
+const randomId = require("./index.js");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.cwxxr.mongodb.net/PhoneBook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  id: randomId(0, 100000),
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (!process.argv[3] && !process.argv[4]) {
+  Person.find({}).then((result) => {
+    console.log("PhoneBook:\n");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+    process.exit(1);
+  });
+} else {
+  person.save().then((result) => {
+    console.log(`Added ${result} to PhoneBook`);
+    mongoose.connection.close();
+  });
+}
